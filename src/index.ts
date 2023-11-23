c9f06838f109a1bdf7fa61f1fe4ad1669f289c37
Pengzhangyan <pengzhengyan@hotmail.com>
2023-11-23 12:04:08 +0800

    Create main page
    Set stage and score panel

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import * as _ from 'lodash'
+import './style/index.less'
+import Snake from './modules/Snake'
+const snake = new Snake()
+// console.log(new Snake)
+
+// function component() {
+//   const element = document.createElement('div')
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+//   return element
+// }
+
+// document.body.appendChild(component())
